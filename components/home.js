515bdcfbfd9214b5d4899684911b38ccda449a03
Andy581 <andy374159@gmail.com>
2022-03-19 18:50:03 -0500

    edited margins of table

--- components/home.js
+++ components/home.js
@@ -46,15 +46,15 @@ const Home = (props) => {
     }
 
     return (
-        <div className="p-4 mx-8">
-            <div className="my-8 flex">
+        <div className="p-4 mx-96">
+            <div className="my-8 flex -mx-36">
                 <input className="border-2 rounded-lg flex-grow p-2 mr-2" placeholder="Search" value={filter} onChange={searchFilter}/>
                 <button className="text-white bg-blue-600 hover:bg-blue-700 cursor-pointer px-4 rounded-lg focus:outline-none">
                     Add Internship
                 </button>
             </div>
             <div className="w-full">
-                <div className="bg-gray-100 rounded-xl py-4 mb-8 shadow-md">
+                <div className="bg-gray-100 rounded-xl py-4 shadow-md">
                     <div className="grid grid-cols-6 text-center">
                         <div>Company</div>
                         <div>Hourly Salary</div>
@@ -67,7 +67,7 @@ const Home = (props) => {
                     {salaries.filter(salary => salary.company.toLowerCase().startsWith(filter.toLowerCase())).map(salary =>
                         <div className="bg-blue-100 rounded-xl grid grid-cols-6 py-2 shadow-md my-6" key={salary.company}>
                             <div className="my-auto">{salary.company}</div>
-                            <div className="my-auto">{salary.salary}</div>
+                            <div className="my-auto">${salary.salary}</div>
                             <div className="my-auto col-span-2">{salary.details}</div>
                             <div className="my-auto">
                                 {salary.experiences.map(experience =>
